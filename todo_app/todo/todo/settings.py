2b754912e8bc5ae02f68b8a1996c52047d09043d
Vrushabh Thite <rishibhthite9322@gmail.com>
2026-03-08 13:53:35 +0530

    added

--- todo_app/todo/todo/settings.py
+++ todo_app/todo/todo/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f(l=udlf2hs1flq$=c^a2)llt(k!d5a#6ck&6filsoxhwwtwxl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+
 
 
 # Application definition
@@ -40,16 +42,18 @@ INSTALLED_APPS = [
     'todo_gem_app',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-
 ]
 
 ROOT_URLCONF = 'todo.urls'
@@ -122,7 +126,11 @@ STATICFILES_DIRS=[
     BASE_DIR/'todo/static'
     ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- todo_app/todo/todo_gem_app/views.py
+++ todo_app/todo/todo_gem_app/views.py
@@ -1,10 +1,107 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Task
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
+
 def addTask(request):
-     task=request.POST['task']
-     Task.objects.create(task=task)
-    #  print(request.POST)
+     if request.method=='POST':
+          task=request.POST.get('task','').strip()
+          if task=='':
+               messages.error(request,'Task cannot be empty!')
+               # print("task cannot be empty")
+               return redirect('home')
+          Task.objects.create(task=task)
+          return redirect('home')
+
+def mark_as_done(request,pk):
+     task=get_object_or_404(Task,pk=pk)
+     #task=Task.objects.get(id=pk)
+     task.is_completed=True
+     task.save()
      return redirect('home')
+
+
+
+def mark_as_undone(request,pk):
+     task=get_object_or_404(Task,pk=pk)
+     task.is_completed=False
+     task.save()
+     return redirect('home')
+
+
+def delet_task(request,pk):
+     task=get_object_or_404(Task,pk=pk)
+     task.delete()
+     return redirect('home')
+
+
+def edit_task(request,pk):
+     get_task=get_object_or_404(Task,pk=pk)
+     if request.method=="POST":
+          new_task=request.POST['task']
+          get_task.task=new_task
+          get_task.save()
+          return redirect('home')
+     else:
+          context={
+               'get_task':get_task,
+          }
+          return render(request,'edit.html',context)
+   
+
+
+# def register_view(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         email = request.POST.get('email')
+#         password1 = request.POST.get('password1')
+#         password2 = request.POST.get('password2')
+
+#         if password1 != password2:
+#             messages.error(request, "Passwords do not match")
+#             return redirect('register')
+
+#         if User.objects.filter(username=username).exists():
+#             messages.error(request, "Username already exists")
+#             return redirect('register')
+
+#         user = User.objects.create_user(
+#             username=username,
+#             email=email,
+#             password=password1
+#         )
+#         user.save()
+#         messages.success(request, "Registration successful")
+#         return redirect('login')
+
+#     return render(request, 'register.html')
+
+
+
+
+# def login_view(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+
+#         user = authenticate(request, username=username, password=password)
+
+#         if user is not None:
+#             login(request, user)
+#             return redirect('home')
+#         else:
+#             messages.error(request, "Invalid username or password")
+#             return redirect('login')
+
+#     return render(request, 'register.html')
+
+
+
+# def logout_view(request):
+#     logout(request)
+#     return redirect('login')
